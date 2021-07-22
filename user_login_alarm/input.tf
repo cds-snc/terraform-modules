ad17f496c2224f018a9d046147530b4044c64fa2
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-07-22 14:51:41 -0400

    fix: handle success/failures and use proper metric
    - Fix the alarm so it uses the metric name not the alarm name
    - Split up the alarm so we have one for success and one for failures
    - Allow the user to set the number of failed attempts before an alarm
      goes off
    - Change the unit to a Count (not sure we actually need to do this)

--- user_login_alarm/input.tf
+++ user_login_alarm/input.tf
@@ -14,7 +14,17 @@ variable "log_group_name" {
   type        = string
 }
 
-variable "alarm_actions" {
-  description = "(required) The list of actions to execute when this alarm transitions to alarm state. Takes a set (array) of ARNs"
+variable "alarm_actions_success" {
+  description = "(required) The list of actions to execute when this success alarm transitions to alarm state. Takes a set (array) of ARNs"
   type        = set(string)
+}
+
+variable "alarm_actions_failure" {
+  description = "(required) The list of actions to execute when this failure alarm transitions to alarm state. Takes a set (array) of ARNs"
+  type        = set(string)
+}
+
+variable "num_attempts" {
+  description = "(required) The number of failed attempts to login before the alarm triggers"
+  type = number
 }
--- user_login_alarm/main.tf
+++ user_login_alarm/main.tf
@@ -1,27 +1,59 @@
-resource "aws_cloudwatch_log_metric_filter" "user_alarm" {
+resource "aws_cloudwatch_log_metric_filter" "user_alarm_success" {
   for_each       = var.account_names
-  name           = "${each.value}_ConsoleLogin"
-  pattern        = "{ $.eventName = \"ConsoleLogin\" && $.userIdentity.userName = \"${each.value}\" }"
+  name           = "${each.value}_ConsoleLogin_Success"
+  pattern        = "{ $.eventName = \"ConsoleLogin\" && $.userIdentity.userName = \"${each.value}\" $$ $.responseElements.ConsoleLogin = \"Success\" }"
   log_group_name = var.log_group_name
 
   metric_transformation {
-    name      = "${each.value}_alarm"
+    # The name of the metric has to be the same as the filter name otherwise the alarm won't work
+    name      = "${each.value}_ConsoleLogin_Success"
     namespace = var.namespace
     value     = 1
+    unit      = "Count"
   }
 
 }
 
-resource "aws_cloudwatch_metric_alarm" "alarm" {
+resource "aws_cloudwatch_metric_alarm" "alarm_success" {
   for_each            = aws_cloudwatch_log_metric_filter.user_alarm
-  alarm_name          = "${each.value.name}_alarm"
+  alarm_name          = "${each.value.name}_alarm_success"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = "1"
-  period              = 10
+  period              = 300
   metric_name         = each.value.name
   namespace           = var.namespace
-  statistic           = "Maximum"
+  statistic           = "Sum"
   threshold           = 1
-  treat_missing_data  = "ignore"
-  alarm_actions       = var.alarm_actions
+  treat_missing_data  = "notBreaching"
+  alarm_actions       = var.alarm_actions_success
+}
+
+resource "aws_cloudwatch_log_metric_filter" "user_alarm_failure" {
+  for_each       = var.account_names
+  name           = "${each.value}_ConsoleLogin_Failure"
+  pattern        = "{ $.eventName = \"ConsoleLogin\" && $.userIdentity.userName = \"${each.value}\" $$ $.responseElements.ConsoleLogin = \"Success\" }"
+  log_group_name = var.log_group_name
+
+  metric_transformation {
+    # The name of the metric has to be the same as the filter name otherwise the alarm won't work
+    name      = "${each.value}_ConsoleLogin_Failure"
+    namespace = var.namespace
+    value     = 1
+    unit      = "Count"
+  }
+
+}
+
+resource "aws_cloudwatch_metric_alarm" "alarm_failure" {
+  for_each            = aws_cloudwatch_log_metric_filter.user_alarm
+  alarm_name          = "${each.value.name}_alarm_failure"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  period              = 300
+  metric_name         = each.value.name
+  namespace           = var.namespace
+  statistic           = "Sum"
+  threshold           = var.num_attempts
+  treat_missing_data  = "notBreaching"
+  alarm_actions       = var.alarm_actions_failure
 }
