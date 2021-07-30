d49e366a7e77cfc5d7d5a68a7cded9768355658f
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-07-30 15:45:56 -0400

    feat: store the proxy connection string and expose it

--- rds/outputs.tf
+++ rds/outputs.tf
@@ -1,1 +1,8 @@
+output "proxy_connection_string_arn" {
+  description = "The arn for the connectionstring to the RDS proxy"
+  value       = aws_secretsmanager_secret.proxy_connection_string.arn
+}
 
+output "proxy_endpoint" {
+  value = aws_db_proxy.proxy.endpoint
+}
