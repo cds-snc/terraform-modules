22c39e198d5ee3e163ed710313c5f178dc46cd8d
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-07-30 16:53:31 -0400

    fix: expose the secret so we can use it in a lambda

--- rds/outputs.tf
+++ rds/outputs.tf
@@ -3,6 +3,11 @@ output "proxy_connection_string_arn" {
   value       = aws_secretsmanager_secret.proxy_connection_string.arn
 }
 
+output "proxy_connection_string_value" {
+  value     = aws_secretsmanager_secret_version.proxy_connection_string.secret_string
+  sensitive = true
+}
+
 output "proxy_endpoint" {
   value = aws_db_proxy.proxy.endpoint
 }
