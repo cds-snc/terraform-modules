4c74ade8d4182944974b151642e4726752b508b6
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-08-03 10:52:41 -0400

    fix: use postgresql instead of postgres

--- rds/secret.tf
+++ rds/secret.tf
@@ -5,7 +5,7 @@ resource "aws_secretsmanager_secret" "connection_string" {
 
 resource "aws_secretsmanager_secret_version" "connection_string" {
   secret_id     = aws_secretsmanager_secret.connection_string.id
-  secret_string = "postgres://${var.username}:${var.password}@${aws_rds_cluster.cluster.endpoint}/${var.database_name}"
+  secret_string = "postgresql://${var.username}:${var.password}@${aws_rds_cluster.cluster.endpoint}/${var.database_name}"
 }
 
 resource "aws_secretsmanager_secret" "proxy_connection_string" {
@@ -15,5 +15,5 @@ resource "aws_secretsmanager_secret" "proxy_connection_string" {
 
 resource "aws_secretsmanager_secret_version" "proxy_connection_string" {
   secret_id     = aws_secretsmanager_secret.proxy_connection_string.id
-  secret_string = "postgres://${var.username}:${var.password}@${aws_db_proxy.proxy.endpoint}/${var.database_name}"
+  secret_string = "postgresql://${var.username}:${var.password}@${aws_db_proxy.proxy.endpoint}/${var.database_name}"
 }
