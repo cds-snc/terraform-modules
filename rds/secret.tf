7126f10bf82d0af4c27578ba8ceb2194d0d28020
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-08-04 14:51:58 -0400

    fix: use straight json

--- rds/secret.tf
+++ rds/secret.tf
@@ -4,19 +4,14 @@ resource "aws_secretsmanager_secret" "connection_string" {
 }
 
 
-variable "secret_credentials" {
-  default = {
-    username = "${var.username}"
-    password = "${var.password}"
-  }
-
-  type = map(string)
-}
 
 // Secret format grabbed from here: https://docs.aws.amazon.com/AmazonRDS/latest/AuroraUserGuide/rds-proxy.html#rds-proxy-secrets-arns
 resource "aws_secretsmanager_secret_version" "connection_string" {
   secret_id     = aws_secretsmanager_secret.connection_string.id
-  secret_string = jsonencode(var.secret_credentials)
+  secret_string = jsonencode({
+    username = "${var.username}",
+    password = "${var.password}"
+  })
 }
 
 resource "aws_secretsmanager_secret" "proxy_connection_string" {
