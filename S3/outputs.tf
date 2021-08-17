d7c8c18440ef8b7c4f8868660478286348667777
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-08-17 11:40:33 -0400

    fix: remove redundant bucket

--- S3/outputs.tf
+++ S3/outputs.tf
@@ -8,12 +8,12 @@ output "s3_bucket_arn" {
   value       = aws_s3_bucket.this.arn
 }
 
-output "s3_bucket_bucket_domain_name" {
+output "s3_bucket_domain_name" {
   description = "The bucket domain name. Will be of format bucketname.s3.amazonaws.com."
   value       = aws_s3_bucket.this.bucket_domain_name
 }
 
-output "s3_bucket_bucket_regional_domain_name" {
+output "s3_bucket_regional_domain_name" {
   description = "The bucket region-specific domain name. The bucket domain name including the region name, please refer here for format. Note: The AWS CloudFront allows specifying S3 region-specific endpoint when creating S3 origin, it will prevent redirect issues from CloudFront to S3 Origin URL."
   value       = aws_s3_bucket.this.bucket_regional_domain_name
 }
