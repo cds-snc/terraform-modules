ef8bd59ef93f5dc398862780b5475e1609b6670d
Pat Heard <patrick.heard@cds-snc.ca>
2021-08-18 15:08:21 +0000

    fix: remove unnecessary named import

--- vpc/test/vpc_test.go
+++ vpc/test/vpc_test.go
@@ -3,14 +3,14 @@ package test
 import (
 	"testing"
 
-	awssdk "github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/stretchr/testify/require"
 )
 
 // Gets a VPCs internet gateway
 func GetVpcInternetGateways(t *testing.T, client *ec2.EC2, vpcId string) *ec2.DescribeInternetGatewaysOutput {
-	igwFilter := ec2.Filter{Name: awssdk.String("attachment.vpc-id"), Values: []*string{&vpcId}}
+	igwFilter := ec2.Filter{Name: aws.String("attachment.vpc-id"), Values: []*string{&vpcId}}
 
 	igws, err := client.DescribeInternetGateways(&ec2.DescribeInternetGatewaysInput{Filters: []*ec2.Filter{&igwFilter}})
 	require.NoError(t, err)
@@ -20,7 +20,7 @@ func GetVpcInternetGateways(t *testing.T, client *ec2.EC2, vpcId string) *ec2.De
 
 // Gets a VPCs NAT gateways
 func GetVpcNatGateways(t *testing.T, client *ec2.EC2, vpcId string) *ec2.DescribeNatGatewaysOutput {
-	vpcFilter := ec2.Filter{Name: awssdk.String("vpc-id"), Values: []*string{&vpcId}}
+	vpcFilter := ec2.Filter{Name: aws.String("vpc-id"), Values: []*string{&vpcId}}
 
 	natGateways, err := client.DescribeNatGateways(&ec2.DescribeNatGatewaysInput{Filter: []*ec2.Filter{&vpcFilter}})
 	require.NoError(t, err)
@@ -30,10 +30,10 @@ func GetVpcNatGateways(t *testing.T, client *ec2.EC2, vpcId string) *ec2.Describ
 
 // Gets the NACLs for a VPC that deny access to port 22 and 3389
 func GetVpcDenyNetworkAcls(t *testing.T, client *ec2.EC2, vpcId string) *ec2.DescribeNetworkAclsOutput {
-	vpcFilter := ec2.Filter{Name: awssdk.String("vpc-id"), Values: []*string{&vpcId}}
-	naclPortSshFilter := ec2.Filter{Name: awssdk.String("entry.port-range.from"), Values: []*string{awssdk.String("22")}}
-	naclPortRdpFilter := ec2.Filter{Name: awssdk.String("entry.port-range.from"), Values: []*string{awssdk.String("3389")}}
-	denyActionFilter := ec2.Filter{Name: awssdk.String("entry.rule-action"), Values: []*string{awssdk.String("deny")}}
+	vpcFilter := ec2.Filter{Name: aws.String("vpc-id"), Values: []*string{&vpcId}}
+	naclPortSshFilter := ec2.Filter{Name: aws.String("entry.port-range.from"), Values: []*string{aws.String("22")}}
+	naclPortRdpFilter := ec2.Filter{Name: aws.String("entry.port-range.from"), Values: []*string{aws.String("3389")}}
+	denyActionFilter := ec2.Filter{Name: aws.String("entry.rule-action"), Values: []*string{aws.String("deny")}}
 
 	networkAcls, err := client.DescribeNetworkAcls(&ec2.DescribeNetworkAclsInput{Filters: []*ec2.Filter{&vpcFilter, &naclPortSshFilter, &naclPortRdpFilter, &denyActionFilter}})
 	require.NoError(t, err)
@@ -43,7 +43,7 @@ func GetVpcDenyNetworkAcls(t *testing.T, client *ec2.EC2, vpcId string) *ec2.Des
 
 // Gets a VPCs flow log
 func GetVpcFlowLogs(t *testing.T, client *ec2.EC2, logGroupName string) *ec2.DescribeFlowLogsOutput {
-	flowLogsFilter := ec2.Filter{Name: awssdk.String("log-group-name"), Values: []*string{&logGroupName}}
+	flowLogsFilter := ec2.Filter{Name: aws.String("log-group-name"), Values: []*string{&logGroupName}}
 
 	flowLogs, err := client.DescribeFlowLogs(&ec2.DescribeFlowLogsInput{Filter: []*ec2.Filter{&flowLogsFilter}})
 	require.NoError(t, err)
