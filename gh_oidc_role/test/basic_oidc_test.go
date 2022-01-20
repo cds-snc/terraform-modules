78bf5f1715354b9aeffc1031a61495845fc0f15f
Mohamed Nur <mohamed.nur@cds-snc.ca>
2022-01-20 14:03:51 -0500

    fix: run tests sequentially and retry

--- gh_oidc_role/test/basic_oidc_test.go
+++ gh_oidc_role/test/basic_oidc_test.go
@@ -2,20 +2,32 @@ package test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
 func TestBasicOIDCRoleCreation(t *testing.T) {
-	t.Parallel()
 
 	region := "ca-central-1"
+
+	// There is a delay from when terraform deletes the oidc provider and when
+	// it disappears from AWS.  These retries will allow the creation of the oidc
+	// provider to eventually succeed.
+	maxRetries := 3
+	timeBetweenRetries := 10 * time.Second
+	retryableTerraformErrors := map[string]string{
+		".*error creating IAM OIDC Provider*": "EntityAlreadyExists*",
+	}
 
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../examples/basic_oidc",
 		EnvVars: map[string]string{
 			"AWS_DEFAULT_REGION": region,
 		},
+		MaxRetries:               maxRetries,
+		TimeBetweenRetries:       timeBetweenRetries,
+		RetryableTerraformErrors: retryableTerraformErrors,
 	}
 
 	// Destroy resource once tests are finished
--- gh_oidc_role/test/oidc_add_assume_test.go
+++ gh_oidc_role/test/oidc_add_assume_test.go
@@ -2,20 +2,32 @@ package test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
 func TestOIDCWithAdditionalAssumeRoleCreation(t *testing.T) {
-	t.Parallel()
 
 	region := "ca-central-1"
+
+	// There is a delay from when terraform deletes the oidc provider and when
+	// it disappears from AWS.  These retries will allow the creation of the oidc
+	// provider to eventually succeed.
+	maxRetries := 3
+	timeBetweenRetries := 10 * time.Second
+	retryableTerraformErrors := map[string]string{
+		".*error creating IAM OIDC Provider*": "EntityAlreadyExists*",
+	}
 
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../examples/oidc_add_assume",
 		EnvVars: map[string]string{
 			"AWS_DEFAULT_REGION": region,
 		},
+		MaxRetries:               maxRetries,
+		TimeBetweenRetries:       timeBetweenRetries,
+		RetryableTerraformErrors: retryableTerraformErrors,
 	}
 
 	// Destroy resource once tests are finished
