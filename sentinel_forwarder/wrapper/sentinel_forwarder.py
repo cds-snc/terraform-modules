8063dd934603215b61c68548fcd5c0a038d7dbfb
Max Neuvians <max.neuvians@cds-snc.ca>
2022-02-07 21:03:23 +0000

    fix: syntax

--- sentinel_forwarder/wrapper/sentinel_forwarder.py
+++ sentinel_forwarder/wrapper/sentinel_forwarder.py
@@ -3,7 +3,7 @@ import connector
 def lambda_handler(event, context):
     try:
         # Passes the event to the Sentinel connector layer
-        connector.handle_log(event):
+        connector.handle_log(event)
         return True
     except Exception as e:
         print(e)
