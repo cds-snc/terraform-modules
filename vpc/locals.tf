bd49d46589f7a88136d37948757de47afb198800
Mohamed Nur <mohamed.nur@cds-snc.ca>
2022-05-12 16:24:08 -0400

    fix: grouping

--- vpc/locals.tf
+++ vpc/locals.tf
@@ -5,7 +5,7 @@ locals {
   }
   zone_names        = var.high_availability ? data.aws_availability_zones.available.names : [data.aws_availability_zones.available.names[0]]
   max_subnet_length = length(local.zone_names)
-  nat_gateway_count = var.enable_eip ? var.single_nat_gateway ? 1 : local.max_subnet_length : 0
+  nat_gateway_count = var.enable_eip ? (var.single_nat_gateway ? 1 : local.max_subnet_length) : 0
   nat_gateway_ips   = var.enable_eip ? tolist(aws_eip.nat.*.id) : [""]
 }
 
