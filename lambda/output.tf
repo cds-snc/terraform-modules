84e5b457278f682c48bc041febc1161559cd619f
Pat Heard <patrick.heard@cds-snc.ca>
2022-06-30 09:48:20 -0400

    feat: Lambda function role ARN output (#134)
    Add the Lambda function role ARN to the module's outputs.
    This ARN can then be used for IAM resource and permission
    policies.

--- lambda/output.tf
+++ lambda/output.tf
@@ -1,11 +1,19 @@
 output "function_arn" {
-  value = aws_lambda_function.this.arn
+  description = "ARN of the Lambda function."
+  value       = aws_lambda_function.this.arn
 }
 
 output "function_name" {
-  value = aws_lambda_function.this.function_name
+  description = "Name of the Lambda function."
+  value       = aws_lambda_function.this.function_name
+}
+
+output "function_role_arn" {
+  description = "ARN of the Lambda function execution role."
+  value       = aws_iam_role.this.arn
 }
 
 output "invoke_arn" {
-  value = aws_lambda_function.this.invoke_arn
+  description = "ARN used to invoke the Lambda function."
+  value       = aws_lambda_function.this.invoke_arn
 }
