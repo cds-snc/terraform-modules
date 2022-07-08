01640ea5a7c62d51121a05ba37101269db7a67a8
Pat Heard <patrick.heard@cds-snc.ca>
2022-07-08 15:09:51 -0400

    feat: add `RequestId` to forwarded event payload (#140)
    Add a `RequestId` UUID to the event used to invoke the `s3-scan-object`
    lambda.  This will be used to track requests as they flow through the
    different components of the system.

--- S3_scan_object/lambda/main_test.py
+++ S3_scan_object/lambda/main_test.py
@@ -4,11 +4,12 @@ from unittest.mock import patch
 
 @patch("main.client")
 @patch("main.json.loads")
+@patch("main.uuid.uuid4", return_value="1234asdf-5678-ghjk-9012-qwer12324poiy5678")
 @patch("main.ACCOUNT_ID", "GeneralKenobi")
 @patch("main.S3_SCAN_OBJECT_FUNCTION_ARN", "arn:aws:lambda:ca-central-1:123456789012:function:anakin")
-def test_handler(mock_json_loads, mock_client):
+def test_handler(mock_uuid, mock_json_loads, mock_client):
   main.handler({"Hello": "There"}, None)
   mock_client.invoke.assert_called_with(
     FunctionName='arn:aws:lambda:ca-central-1:123456789012:function:anakin', 
-    Payload='{"Hello": "There", "AccountId": "GeneralKenobi"}'
+    Payload='{"Hello": "There", "AccountId": "GeneralKenobi", "RequestId": "1234asdf-5678-ghjk-9012-qwer12324poiy5678"}'
   )
