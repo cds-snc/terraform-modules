cdf9bb212394c8a99041d3e8e028d4bffc73e45e
Pat Heard <patrick.heard@cds-snc.ca>
2022-08-31 20:14:41 +0000

    chore: stay at TF version 1.1.0

--- .github/workflows/conftest/test_plan.tf
+++ .github/workflows/conftest/test_plan.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.2.0"
+  required_version = "~> 1.1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -99,9 +99,9 @@ module "oicd_role" {
   source            = "../../../gh_oidc_role"
   billing_tag_value = "cal"
   roles = [{
-    name = "test"
+    name      = "test"
     repo_name = "foo"
-    claim = "*"
+    claim     = "*"
 
   }]
 }
