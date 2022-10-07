c7bdd01e73359df6cefccbc22622163aa9f46216
Max Neuvians <max@neuvians.net>
2022-10-07 11:48:25 -0400

    Update input.tf

--- sentinel_forwarder/input.tf
+++ sentinel_forwarder/input.tf
@@ -39,7 +39,7 @@ variable "function_name" {
 
 variable "layer_arn" {
   description = "(Optional) ARN of the lambda layer to use"
-  default     = "arn:aws:lambda:ca-central-1:283582579564:layer:aws-sentinel-connector-layer:8"
+  default     = "arn:aws:lambda:ca-central-1:283582579564:layer:aws-sentinel-connector-layer:9"
 }
 
 variable "log_type" {
