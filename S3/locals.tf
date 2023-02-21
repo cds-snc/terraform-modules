ab1020eef2afd2ba9da642958e20941cd4542a5f
Guillaume Charest <guillaume.charest@cds-snc.ca>
2023-02-21 15:50:34 +0000

    feat: add critical_tag to common_tags

--- S3/locals.tf
+++ S3/locals.tf
@@ -1,6 +1,7 @@
 locals {
   common_tags = {
-    (var.billing_tag_key) = var.billing_tag_value
-    Terraform             = "true"
+    (var.billing_tag_key)  = var.billing_tag_value
+    Terraform              = "true"
+    (var.critical_tag_key) = var.critical_tag_value ? "true" : "false"
   }
 }
