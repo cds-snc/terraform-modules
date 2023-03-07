6821cf252f29ab2f0f8c56eaa7416f5b8c78e3dc
omartehsin1 <omartehsin@gmail.com>
2023-03-07 15:27:41 -0500

    updated README

--- lambda/input.tf
+++ lambda/input.tf
@@ -84,7 +84,7 @@ variable "ephemeral_storage" {
 variable "file_system_config" {
   type = map(string)
   default = {}
-  description = "(Optional) Configuration to connect EFS to a Lambda function"
+  description = "(Optional) Configuration to connect EFS to a Lambda function."
 }
 
 variable "image_uri" {
