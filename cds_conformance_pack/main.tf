0f57fe531b0a3f71131fb7b5aa98e08004da3d1e
Max Neuvians <max@neuvians.net>
2023-04-21 17:15:21 +0000

    feat: remove config

--- cds_conformance_pack/main.tf
+++ cds_conformance_pack/main.tf
@@ -127,29 +127,4 @@ resource "aws_config_conformance_pack" "cds_conformance_pack" {
   }
 
   template_body = file("${path.module}/Operational-Best-Practices-for-CCCS-Medium.yaml")
-
-  depends_on = [aws_config_configuration_recorder.cds_conformance_pack]
-}
-
-resource "aws_config_configuration_recorder" "cds_conformance_pack" {
-  name     = "cds_conformance_pack"
-  role_arn = aws_iam_role.cds_conformance_pack.arn
-}
-
-data "aws_iam_policy_document" "cds_conformance_pack_assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["config.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "cds_conformance_pack" {
-  name               = "cds_conformance_pack"
-  assume_role_policy = data.aws_iam_policy_document.cds_conformance_pack_assume_role.json
 }
