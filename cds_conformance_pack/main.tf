07f292b5cb20b51b46e6a64f034e2c639fc2b3e2
Max Neuvians <max@neuvians.net>
2023-04-21 18:42:44 +0000

    fix: docs

--- cds_conformance_pack/main.tf
+++ cds_conformance_pack/main.tf
@@ -9,7 +9,7 @@
 * 
 * ```hcl
 * module "conformance_pack" {
-*   source                                                        = "github.com/cds-snc/terraform-modules?ref=v5.1.5/cds_conformance_pack"
+*   source                                                        = "github.com/cds-snc/terraform-modules?ref=v5.1.6/cds_conformance_pack"
 *   internet_gateway_authorized_vpc_only_param_authorized_vpc_ids = "vpc-00534274da4ade29d"
 *   billing_tag_value = var.billing_code
 * }
