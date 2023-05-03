83ec408488245fea6b38bd001a8eddf3d0c215e0
Guillaume Charest <guillaume.charest@cds-snc.ca>
2023-05-03 14:04:15 +0000

    feat: add common tag to ID log_buckets

--- S3/locals.tf
+++ S3/locals.tf
@@ -2,6 +2,7 @@ locals {
   common_tags = {
     (var.billing_tag_key)  = var.billing_tag_value
     Terraform              = "true"
+    Log_Bucket             = lookup(var.tags, "Log_Bucket", "false")
     (var.critical_tag_key) = var.critical_tag_value ? "true" : "false"
   }
 }
