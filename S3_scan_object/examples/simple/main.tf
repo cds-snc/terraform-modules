46221c255d95b20bb8fbf469bad47df73d518ee6
Pat Heard <patrick.heard@cds-snc.ca>
2023-05-29 19:06:12 +0000

    feat: update s3_scan_object to use an SQS queue
    Update the module to replace the transport lambda with an SQS
    queue that is monitored cross-account by the Scan Files API.
    
    This is being done to simplify the architecture of the async
    scanning process as we are seeing timeouts in Notify which
    has a spikey use of the API.

--- S3_scan_object/examples/simple/main.tf
+++ S3_scan_object/examples/simple/main.tf
@@ -1,24 +1,8 @@
 module "simple" {
   source = "../../"
 
-  product_name          = "simple"
   s3_upload_bucket_name = module.upload_bucket.s3_bucket_id
-
-  alarm_on_lambda_error     = true
-  alarm_ok_sns_topic_arn    = aws_sns_topic.cloudwatch_alarms_ok.arn
-  alarm_error_sns_topic_arn = aws_sns_topic.cloudwatch_alarms_chaos_and_madness.arn
-
-  billing_tag_value = "terratest"
-
-  log_level = "DEBUG"
-}
-
-resource "aws_sns_topic" "cloudwatch_alarms_ok" {
-  name = "cloudwatch_alarms_ok"
-}
-
-resource "aws_sns_topic" "cloudwatch_alarms_chaos_and_madness" {
-  name = "cloudwatch_alarms_chaos_and_madness"
+  billing_tag_value     = "terratest"
 }
 
 resource "random_id" "upload_bucket" {
---
+++ S3_scan_object/kms.tf
@@ -0,0 +1,43 @@
+resource "aws_kms_key" "s3_scan_object_queue" {
+  description         = "S3 scan objects KMS key for SQS queue encryption"
+  policy              = data.aws_iam_policy_document.s3_scan_object_queue.json
+  enable_key_rotation = true
+  tags                = local.common_tags
+}
+
+resource "aws_kms_alias" "s3_scan_object_queue" {
+  name          = "alias/s3_scan_object_queue"
+  target_key_id = aws_kms_key.s3_scan_object_queue.key_id
+}
+
+data "aws_iam_policy_document" "s3_scan_object_queue" {
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+    actions   = ["kms:*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${local.account_id}:root"]
+    }
+  }
+
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey*",
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+
+    principals {
+      type        = "AWS"
+      identifiers = [var.s3_scan_object_role_arn]
+    }
+  }
+}
