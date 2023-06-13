99f24dfff4305e33da247386ab939b50f77ff0a6
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2023-06-13 15:54:07 +0000

    fix: set proper values for Log_Bucket tags

--- S3_log_bucket/locals.tf
+++ S3_log_bucket/locals.tf
@@ -2,7 +2,7 @@ locals {
   common_tags = {
     (var.billing_tag_key)  = var.billing_tag_value
     Terraform              = "true"
-    Log_Bucket             = lookup(var.tags, "Log_Bucket", "true")
+    Log_Bucket             = "true"
     (var.critical_tag_key) = var.critical_tag_value ? "true" : "false"
   }
 }
