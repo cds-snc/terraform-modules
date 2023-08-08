93f7fd66e654df165a9dc8cca2ec772a7eac3d53
Guillaume Charest <guillaume.charest@cds-snc.ca>
2023-08-08 18:09:09 +0000

    feat: setup sentinel forwarder option
    Signed-off-by: Guillaume Charest <guillaume.charest@cds-snc.ca>

--- S3_log_bucket/locals.tf
+++ S3_log_bucket/locals.tf
@@ -4,4 +4,14 @@ locals {
     Terraform              = "true"
     (var.critical_tag_key) = var.critical_tag_value ? "true" : "false"
   }
+  sentinel_forwarder = var.configure_sentinel_forwarder ? {
+    function_name     = "${var.bucket_name}-logs-sentinel-forwarder"
+    billing_tag_key   = var.billing_tag_key
+    billing_tag_value = var.billing_tag_value
+    log_type          = "AWSS3ServerAccessLogs"
+    bucket_arn        = aws_s3_bucket.this.arn
+    bucket_id         = aws_s3_bucket.this.id
+    filter_prefix     = "logs/"
+    kms_key_arn       = ""
+  } : {}
 }
