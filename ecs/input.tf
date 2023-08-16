b0966a130dd635c23498911b38e7e913e91a91bb
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2023-08-16 16:46:13 -0700

    Adding service information to terraform module

--- ecs/input.tf
+++ ecs/input.tf
@@ -1,4 +1,57 @@
+################################################################################
+# Cluster
+################################################################################
 
+variable "cluster_name" {
+  description = "Name of the cluster (up to 255 letters, numbers, hyphens, and underscores)"
+  type        = string
+  default     = ""
+}
+
+variable "cluster_settings" {
+  description = "Configuration block(s) with cluster settings."
+  type        = map(string)
+  default = {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+
+################################################################################
+# CloudWatch Log Group
+################################################################################
+
+variable "create_cloudwatch_log_group" {
+  description = "Determines whether a log group is created by this module for the cluster logs. If not, AWS will automatically create one if logging is enabled"
+  type        = bool
+  default     = true
+}
+
+variable "cloudwatch_log_group_retention_in_days" {
+  description = "Number of days to retain log events"
+  type        = number
+  default     = 90
+}
+
+################################################################################
+# Service
+################################################################################
+
+variable "desired_count" {
+  description = "The number of instances of the task definition to place and keep running. Defaults to `0`"
+  type        = number
+  default     = 1
+}
+
+variable "service_name" {
+  description = "Name of the service (up to 255 letters, numbers, hyphens, and underscores)"
+  type        = string
+  default     = null
+}
+
+################################################################################
+# Common
+################################################################################
 variable "billing_tag_key" {
   description = "(Optional, default 'CostCentre') The name of the billing tag"
   type        = string
@@ -8,5 +61,11 @@ variable "billing_tag_key" {
 variable "billing_tag_value" {
   description = "(Required) The value of the billing tag"
   type        = string
+}
+
+variable "launch_type" {
+  description = "Launch type on which to run your service. The valid values are `EC2`, `FARGATE`, and `EXTERNAL`. Defaults to `FARGATE`"
+  type        = string
+  default     = "FARGATE"
 }
 
--- ecs/main.tf
+++ ecs/main.tf
@@ -38,3 +38,40 @@ resource "aws_cloudwatch_log_group" "log_group" {
     Name = "${var.name}_log_group"
   })
 }
+
+################################################################################
+# Service
+################################################################################
+resource "aws_ecs_service" "ecs_service" {
+  name            = var.service_name
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.ecs_task_definition.arn
+  desired_count   = var.desired_count
+  launch_type     = "FARGATE"
+
+  dynamic "network_configuration" {
+    for_each = [var.network_configuration]
+
+    content {
+      subnets         = network_configuration.value.subnets
+      security_groups = network_configuration.value.security_groups
+      assign_public_ip = network_configuration.value.assign_public_ip
+    }
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.ecs_target_group.arn
+    container_name   = var.container_name
+    container_port   = var.container_port
+  }
+
+  lifecycle {
+    ignore_changes = [
+      task_definition
+    ]
+  }
+
+  tags = merge(local.common_tags, {
+    Name = "${var.name}_ecs_service"
+  })
+}
