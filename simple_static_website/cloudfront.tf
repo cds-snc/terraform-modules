d715582b2de62bc37ea65213f9bc05c19312422f
Max Neuvians <max@neuvians.net>
2023-10-18 00:30:04 +0000

    feat: add SPA config

--- simple_static_website/cloudfront.tf
+++ simple_static_website/cloudfront.tf
@@ -44,6 +44,16 @@ resource "aws_cloudfront_distribution" "simple_static_website" {
     max_ttl                = 86400
   }
 
+  dynamic "custom_error_response" {
+    for_each = var.single_page_app ? [403] : []
+    content {
+      error_code            = 403
+      error_caching_min_ttl = 3600
+      response_code         = 200
+      response_page_path    = var.index_document
+    }
+  }
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
