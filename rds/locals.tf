1c8778515278efc6ca8e07e1ce957917c7744020
Pat Heard <patrick.heard@cds-snc.ca>
2023-10-19 18:34:30 +0000

    fix: RDS copy tags to snapshot, enable logging
    Update the RDS cluster resource to:
    
    1. Copy resource tags to snapshots.
    2. Allow for the export of logs to CloudWatch.
    
    If no logs are selected for export, the module defaults to
    sending `audit` logs for MySQL and `postgresql` for
    Postgres.

--- rds/locals.tf
+++ rds/locals.tf
@@ -4,8 +4,12 @@ locals {
     Terraform             = "true"
   }
 
+  identifier         = "${var.name}-cluster"
   is_mysql           = var.engine == "aurora-mysql"
   database_port      = local.is_mysql ? 3306 : 5432
   engine_family      = local.is_mysql ? "MYSQL" : "POSTGRESQL"
   security_group_ids = distinct(concat([aws_security_group.rds_proxy.id], var.security_group_ids))
+
+  # Configure the database logs that are exported to CloudWatch.  Default to `audit` for MySQL and `postgresql` for Postgres if no values are specified
+  enabled_cloudwatch_logs_exports = length(var.enabled_cloudwatch_logs_exports) ? var.enabled_cloudwatch_logs_exports : [is_mysql ? "audit" : "postgresql"]
 }
