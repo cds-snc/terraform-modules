d011b32b8db8505bf01a065007e4494bb0ebdc30
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2023-10-23 21:59:20 -0700

    Adding principal back in

--- ecs/examples/simple/main.tf
+++ ecs/examples/simple/main.tf
@@ -70,6 +70,11 @@ data "aws_iam_policy_document" "simple" {
   statement {
     actions = ["sts:AssumeRole"]
 
+   principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+
   }
 }
 
