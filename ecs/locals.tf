22a6e22183e2102ee75131c9dd525d94eceaf364
Pat Heard <patrick.heard@cds-snc.ca>
2024-01-30 01:32:55 +0000

    fix: add ECS role ARN outputs
    Add outputs with the ECS execution role ARN and task role ARN.

--- ecs/locals.tf
+++ ecs/locals.tf
@@ -5,4 +5,6 @@ locals {
   }
   container_name         = var.container_name != null ? var.container_name : local.task_definition_family
   task_definition_family = var.task_name != null ? var.task_name : var.service_name
+  task_exec_role_arn     = var.task_exec_role_arn != null ? var.task_exec_role_arn : aws_iam_role.this_task_exec.arn
+  task_role_arn          = var.task_role_arn != null ? var.task_role_arn : aws_iam_role.this_task.arn
 }
