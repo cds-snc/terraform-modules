31550b63dab91a61774771b0c2a9a6c1a633c71b
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2024-03-25 16:36:04 -0700

    Changing the code so that it is clear that the webhook should be passed as a variable and not hard coded into the file so that it does not get leaked

--- spend_notifier/examples/main.tf
+++ spend_notifier/examples/main.tf
@@ -1,7 +1,7 @@
 module "spend_notifier_example" {
   source                     = "github.com/cds-snc/terraform-modules//spend_notifier?ref=v9.2.5"
-  daily_spend_notifier_hook  = "d5bfc2dc-07d9-4553-adbb-08a0b44a4bea"
-  weekly_spend_notifier_hook = "d5bfc2dc-07d9-4553-adbb-08a0b44a4bea"
+  daily_spend_notifier_hook  = var.webhook_url
+  weekly_spend_notifier_hook = var.webhook_url 
   billing_tag_value          = "My billing tag value"
   account_name               = "My account name"
 }
