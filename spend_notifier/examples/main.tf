c9aab5fcb36fcb98e1f124f578a0357e0c062aa0
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2024-04-08 22:18:50 +0000

    Making the spend notifier more flexible by adding additional inputs

--- spend_notifier/examples/main.tf
+++ spend_notifier/examples/main.tf
@@ -1,7 +1,11 @@
 module "spend_notifier_example" {
-  source                     = "github.com/cds-snc/terraform-modules//spend_notifier?ref=v9.2.7"
-  daily_spend_notifier_hook  = var.daily_spend_notifier_hook
-  weekly_spend_notifier_hook = var.weekly_spend_notifier_hook
-  billing_tag_value          = "My billing tag value"
-  account_name               = "My account name"
+  source                           = "github.com/cds-snc/terraform-modules//spend_notifier?ref=v9.2.7"
+  daily_spend_notifier_hook        = var.daily_spend_notifier_hook
+  weekly_spend_notifier_hook       = var.weekly_spend_notifier_hook
+  billing_tag_value                = "My billing tag value"
+  account_name                     = "My account name"
+  enable_daily_spend_notification  = true
+  enable_weekly_spend_notification = true
+  daily_schedule_expression        = "0 12 * * ? *"
+  weekly_schedule_expression       = "0 12 ? * SUN *"
 }
