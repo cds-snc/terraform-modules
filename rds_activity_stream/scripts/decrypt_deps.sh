3500198d8e0650b43469feccbf2ae5acd746b4c4
Pat Heard <patrick.heard@cds-snc.ca>
2024-04-11 00:54:04 +0000

    test: do not suppress stderr

--- rds_activity_stream/scripts/decrypt_deps.sh
+++ rds_activity_stream/scripts/decrypt_deps.sh
@@ -14,15 +14,15 @@ eval "$(jq -r '@sh "export PYTHON_VERSION=\(.python_version) SRC_DIR=\(.src_dir)
 # Download dependencies
 docker run -v "${SRC_DIR}":/var/task public.ecr.aws/sam/build-${PYTHON_VERSION} \
     /bin/sh -c "pip install -r lambda/requirements.txt -t lambda/layer/python/; exit" \
-    > /dev/null 2>&1
+    > /dev/null
 
 # Generate a deterministic zip file
 # This is done by removing the compiled bytecode and setting the timestamp of the
 # downloaded files to the same date.
-find "${SRC_DIR}/lambda/layer" | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf  > /dev/null 2>&1
-find "${SRC_DIR}/lambda/layer" -exec touch -t 197001010000 {} + > /dev/null 2>&1
+find "${SRC_DIR}/lambda/layer" | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf  > /dev/null
+find "${SRC_DIR}/lambda/layer" -exec touch -t 197001010000 {} + > /dev/null
 cd "${SRC_DIR}/lambda/layer"
-zip -r -X "${SRC_DIR}/lambda/decrypt_deps.zip" python/ > /dev/null 2>&1
+zip -r -X "${SRC_DIR}/lambda/decrypt_deps.zip" python/ > /dev/null
 
 # Produce output
 jq -n \
