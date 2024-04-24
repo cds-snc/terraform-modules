67b3df153f7c8d2a4c55d9889ded53e5faea1029
Pat Heard <patrick.heard@cds-snc.ca>
2024-04-24 12:51:17 +0000

    fix: upgrade to AWS SDK v3
    Update the Lambda code to use the Node.js AWS SDK v3
    which is bundled with Node.js 18+ runtime environments.
    
    Update the Lambda runtime to Node.js 20.

--- spend_notifier/examples/main.tf
+++ spend_notifier/examples/main.tf
@@ -1,9 +1,9 @@
 module "spend_notifier_example" {
-  source                           = "github.com/cds-snc/terraform-modules//spend_notifier?ref=v9.3.8"
+  source                           = "../"
   daily_spend_notifier_hook        = var.daily_spend_notifier_hook
   weekly_spend_notifier_hook       = var.weekly_spend_notifier_hook
   billing_tag_value                = "My billing tag value"
-  account_name                     = "My account name"
+  account_name                     = "123456789012"
   enable_daily_spend_notification  = true
   enable_weekly_spend_notification = true
   daily_schedule_expression        = "0 12 * * ? *"
