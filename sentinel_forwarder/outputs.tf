c58b38dc2733af1092fba1616fcf8608cabccb3d
Pat Heard <patrick.heard@cds-snc.ca>
2024-04-24 14:22:50 -0400

    feat: add Sentinel forwarder function name output (#470)
    Add the Sentinel forwarder Lambda function name as a new output.

--- sentinel_forwarder/outputs.tf
+++ sentinel_forwarder/outputs.tf
@@ -2,3 +2,8 @@ output "lambda_arn" {
   description = "The ARN of the Lambda function."
   value       = aws_lambda_function.sentinel_forwarder.arn
 }
+
+output "lambda_name" {
+  description = "The name of the Lambda function."
+  value       = aws_lambda_function.sentinel_forwarder.function_name
+}
