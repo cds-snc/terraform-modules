0d2c0b123ef1b0786f9616dd5401bd6c48eebdf3
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2024-05-24 17:56:11 +0000

    Adding an explicit effect since the opa checks expect an explicit allow or deny

--- simple_static_website/main.tf
+++ simple_static_website/main.tf
@@ -34,6 +34,7 @@ terraform {
 data "aws_iam_policy_document" "s3_policy" {
   statement {
     actions   = ["s3:GetObject"]
+    effect    = "Allow"
     resources = ["${aws_s3_bucket.this.arn}/*"]
 
     principals {
