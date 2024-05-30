25981537b678133770131323a7ece270271d4d97
Guillaume Charest <guillaume.charest@cds-snc.ca>
2024-05-30 18:46:29 +0000

    feat: add endpoint as output

--- simple_static_website/outputs.tf
+++ simple_static_website/outputs.tf
@@ -12,3 +12,8 @@ output "s3_bucket_region" {
   description = "The AWS region this bucket resides in."
   value       = aws_s3_bucket.this.region
 }
+
+output "s3_bucket_website_endpoint" {
+  description = "The website endpoint for this bucket."
+  value       = aws_s3_bucket_website_configuration.this.website_endpoint
+}
