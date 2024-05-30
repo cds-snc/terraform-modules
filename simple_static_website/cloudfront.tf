d1f581d6853e00024a6c27eb1ad7b151a602265a
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2024-05-30 07:01:41 -0700

    Adding waf to the module (#499)
    * Adding waf to simple_static_website module
    
    * Updating readme
    
    * Update input.tf
    
    Changing the default value to null.
    
    * Updating README
    
    * Updaing the docs and the variable to say that default is null in the description

--- simple_static_website/cloudfront.tf
+++ simple_static_website/cloudfront.tf
@@ -86,5 +86,7 @@ resource "aws_cloudfront_distribution" "simple_static_website" {
     minimum_protocol_version = "TLSv1.2_2021"
   }
 
+  web_acl_id = var.web_acl_arn
+
   tags = local.common_tags
 }
