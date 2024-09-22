28ca81deff6da4867628f685aee2cf531a0d4f6c
Pat Heard <patrick.heard@cds-snc.ca>
2024-09-22 17:41:22 +0000

    fix: fmt

--- waf_ip_blocklist/examples/simple/simple.tf
+++ waf_ip_blocklist/examples/simple/simple.tf
@@ -5,7 +5,7 @@ module "simple" {
   service_name                = "test-app"
   athena_query_results_bucket = "test-app-athena-bucket"
   athena_query_source_bucket  = "test-app-waf-logs-bucket"
-  waf_rule_ids_skip           = ["RuleIdOne","RuleIdTwo"] # These rules will not count towards blocking an IP
+  waf_rule_ids_skip           = ["RuleIdOne", "RuleIdTwo"] # These rules will not count towards blocking an IP
   billing_tag_value           = "test-app"
 }
 
