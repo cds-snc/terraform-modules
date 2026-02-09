98f20402b3fc15c056ae4fc3cc5e8a05fb412d4c
Pat Heard <patrick.heard@cds-snc.ca>
2026-02-09 16:04:31 +0000

    feat: add CodeBuild GitHub runner support for CodeConnection
    Update the CodeBuild runner module to support CodeConnections that use a
    GitHub app to authenticate the runner.  This removes the need for the use of
    a Personal Access Token.

--- codebuild_github_runner/locals.tf
+++ codebuild_github_runner/locals.tf
@@ -1,5 +1,7 @@
 
 locals {
+  is_github_codeconnection = var.github_codeconnection_name != ""
+  is_github_pat            = var.github_personal_access_token != ""
   common_tags = {
     (var.billing_tag_key) = var.billing_tag_value
     Terraform             = "true"
