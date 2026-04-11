91f5cdb74e8fb5cdff760b12a0835cb8daeecc07
Pat Heard <patrick.heard@cds-snc.ca>
2026-04-11 14:01:36 -0400

    fix: create partitioned log tables for WAF/ALB
    Update the create table SQL queries to add partitions. This will allow for
    quicker querying and reduce the chance of getting a HIVE split error.

--- athena_access_logs/sql/waf_create_table.sql
+++ athena_access_logs/sql/waf_create_table.sql
@@ -1,73 +1,42 @@
-CREATE EXTERNAL TABLE `${database_name}.${table_name}`(
+CREATE EXTERNAL TABLE IF NOT EXISTS `${database_name}.${table_name}`(
   `timestamp` bigint, 
   `formatversion` int, 
   `webaclid` string, 
   `terminatingruleid` string, 
   `terminatingruletype` string, 
   `action` string, 
-  `terminatingrulematchdetails` array<
-                                    struct<
-                                        conditiontype:string,
-                                        location:string,
-                                        matcheddata:array<string>
-                                           >
-                                    >, 
+  `terminatingrulematchdetails` array<struct<conditiontype:string,sensitivitylevel:string,location:string,matcheddata:array<string>>>, 
   `httpsourcename` string, 
   `httpsourceid` string, 
-  `rulegrouplist` array<
-                      struct<
-                          rulegroupid:string,
-                          terminatingrule:struct<
-                                              ruleid:string,
-                                              action:string,
-                                              rulematchdetails:string
-                                                >,
-                          nonterminatingmatchingrules:array<string>,
-                          excludedrules:string
-                            >
-                       >, 
- `ratebasedrulelist` array<
-                         struct<
-                             ratebasedruleid:string,
-                             limitkey:string,
-                             maxrateallowed:int
-                               >
-                          >, 
-  `nonterminatingmatchingrules` array<
-                                    struct<
-                                        ruleid:string,
-                                        action:string
-                                          >
-                                     >, 
-  `requestheadersinserted` string, 
+  `rulegrouplist` array<struct<rulegroupid:string,terminatingrule:struct<ruleid:string,action:string,rulematchdetails:array<struct<conditiontype:string,sensitivitylevel:string,location:string,matcheddata:array<string>>>>,nonterminatingmatchingrules:array<struct<ruleid:string,action:string,overriddenaction:string,rulematchdetails:array<struct<conditiontype:string,sensitivitylevel:string,location:string,matcheddata:array<string>>>,challengeresponse:struct<responsecode:string,solvetimestamp:string>,captcharesponse:struct<responsecode:string,solvetimestamp:string>>>,excludedrules:string>>, 
+  `ratebasedrulelist` array<struct<ratebasedruleid:string,limitkey:string,maxrateallowed:int>>, 
+  `nonterminatingmatchingrules` array<struct<ruleid:string,action:string,rulematchdetails:array<struct<conditiontype:string,sensitivitylevel:string,location:string,matcheddata:array<string>>>,challengeresponse:struct<responsecode:string,solvetimestamp:string>,captcharesponse:struct<responsecode:string,solvetimestamp:string>>>, 
+  `requestheadersinserted` array<struct<name:string,value:string>>, 
   `responsecodesent` string, 
-  `httprequest` struct<
-                    clientip:string,
-                    country:string,
-                    headers:array<
-                                struct<
-                                    name:string,
-                                    value:string
-                                      >
-                                 >,
-                    uri:string,
-                    args:string,
-                    httpversion:string,
-                    httpmethod:string,
-                    requestid:string
-                      >, 
-  `labels` array<
-               struct<
-                   name:string
-                     >
-                >, 
-  `captcharesponse` struct<
-                        responsecode:string,
-                        solvetimestamp:string,
-                        failureReason:string
-                          > 
-)
-ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
-STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat'
-OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
-LOCATION '${bucket_location}'
+  `httprequest` struct<clientip:string,country:string,headers:array<struct<name:string,value:string>>,uri:string,args:string,httpversion:string,httpmethod:string,requestid:string,fragment:string,scheme:string,host:string>,
+  `labels` array<struct<name:string>>, 
+  `captcharesponse` struct<responsecode:string,solvetimestamp:string,failurereason:string>, 
+  `challengeresponse` struct<responsecode:string,solvetimestamp:string,failurereason:string>, 
+  `ja3fingerprint` string, 
+  `ja4fingerprint` string, 
+  `oversizefields` string, 
+  `requestbodysize` int, 
+  `requestbodysizeinspectedbywaf` int)
+  PARTITIONED BY ( 
+   `hour` string)
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  '${bucket_location}'
+TBLPROPERTIES (
+  'projection.enabled'='true',
+  'projection.hour.format'='yyyy/MM/dd/HH',
+  'projection.hour.interval'='1',
+  'projection.hour.interval.unit'='hours',
+  'projection.hour.range'='2020/01/01/00,NOW',
+  'projection.hour.type'='date',
+  'storage.location.template'='${bucket_location}/${hour}')
