def51983d852b6894f8501215e249881cdc43f5d
Omar Tehsin <omartehsin@gmail.com>
2026-04-21 13:41:24 -0400

    fixed scanning

--- waf_ip_blocklist/lambda/query_lb.sql
+++ waf_ip_blocklist/lambda/query_lb.sql
@@ -11,6 +11,7 @@ WHERE
         OR target_status_code LIKE '5__'
     )
     AND target_status_code NOT IN ({skip_list})
+    AND day IN ({day_partition_filter})
     AND from_iso8601_timestamp(time) >= date_add('day', -1, current_timestamp)
 GROUP BY
     client_ip
