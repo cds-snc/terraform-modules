6a47fac84fa9fcc7e972e5a2e863f8338e4ce64f
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2026-05-08 14:24:51 -0700

    Formatting locals file

--- S3/locals.tf
+++ S3/locals.tf
@@ -1,8 +1,8 @@
 locals {
   common_tags = {
-    (var.billing_tag_key)    = var.billing_tag_value
-    (var.ssc_cbrid_tag_key)  = var.ssc_cbrid_tag_value
-    Terraform                = "true"
-    (var.critical_tag_key)   = var.critical_tag_value ? "true" : "false"
+    (var.billing_tag_key)   = var.billing_tag_value
+    (var.ssc_cbrid_tag_key) = var.ssc_cbrid_tag_value
+    Terraform               = "true"
+    (var.critical_tag_key)  = var.critical_tag_value ? "true" : "false"
   }
 }
