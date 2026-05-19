486e012d5ddb7cc7b980c7ae74a63517f66a662d
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2026-05-19 16:16:28 -0700

    Adding core services cbr id tags to core services components

--- guardduty/locals.tf
+++ guardduty/locals.tf
@@ -1,10 +1,11 @@
 
 locals {
-  common_tags = {
-    (var.billing_tag_key) = var.billing_tag_value
-    Terraform             = "true"
-  }
-
-  # tags = merge(var.tags, common_tags)
+  common_tags = merge(
+    {
+      (var.billing_tag_key) = var.billing_tag_value
+      Terraform             = "true"
+    },
+    var.ssc_cbrid_tag_value != "" ? { (var.ssc_cbrid_tag_key) = var.ssc_cbrid_tag_value } : {}
+  )
 }
 
