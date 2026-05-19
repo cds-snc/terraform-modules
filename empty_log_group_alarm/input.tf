c18ab6bb1cb6d22d6e7255afda0e629728328e87
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2026-05-19 13:54:06 -0700

    Adding ssc cbrid tags to the core services components

--- empty_log_group_alarm/input.tf
+++ empty_log_group_alarm/input.tf
@@ -15,6 +15,18 @@ variable "billing_tag_value" {
   type        = string
 }
 
+variable "ssc_cbrid_tag_key" {
+  description = "(Optional) The name of the SSC CBRID tag"
+  type        = string
+  default     = "ssc_cbrid"
+}
+
+variable "ssc_cbrid_tag_value" {
+  description = "(Optional) The value of the SSC CBRID tag. If empty, the SSC CBRID tag will not be applied."
+  type        = string
+  default     = "22DH"
+}
+
 variable "log_group_names" {
   description = "(Required) The list of log group names to monitor"
   type        = list(string)
