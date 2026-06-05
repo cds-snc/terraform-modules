ab74d0648eb5e109c31c76ad1e1548088e92f5ab
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2026-06-05 07:17:35 -0700

    Adding core services tags to ECS task (#860)

--- ecs/locals.tf
+++ ecs/locals.tf
@@ -1,9 +1,12 @@
 locals {
   cloudwatch_log_group_name = var.cloudwatch_log_group_append_service_name ? "/aws/ecs/${var.cluster_name}/${var.service_name}" : "/aws/ecs/${var.cluster_name}"
-  common_tags = {
-    (var.billing_tag_key) = var.billing_tag_value
-    Terraform             = true
-  }
+  common_tags = merge(
+    {
+      (var.billing_tag_key) = var.billing_tag_value
+      Terraform             = true
+    },
+    var.ssc_cbrid_tag_value != "" ? { (var.ssc_cbrid_tag_key) = var.ssc_cbrid_tag_value } : {}
+  )
   container_name         = var.container_name != null ? var.container_name : local.task_definition_family
   task_definition_family = var.task_name != null ? var.task_name : var.service_name
   task_exec_role_arn     = var.task_exec_role_arn != null ? var.task_exec_role_arn : aws_iam_role.this_task_exec.arn
