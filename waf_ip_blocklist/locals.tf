b237853f745647c0c27749c96a9c9ed4d497b237
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2026-06-05 06:25:03 -0700

    Adding core services tags to waf ip blocklist module (#882)

--- waf_ip_blocklist/locals.tf
+++ waf_ip_blocklist/locals.tf
@@ -4,8 +4,11 @@ locals {
   athena_query_source_bucket_arn  = "arn:aws:s3:::${var.athena_query_source_bucket}"
   blocklist_name                  = "ipv4_blocklist_${var.service_name}"
 
-  common_tags = {
-    (var.billing_tag_key) = var.billing_tag_value
-    Terraform             = "true"
-  }
+  common_tags = merge(
+    {
+      (var.billing_tag_key) = var.billing_tag_value
+      Terraform             = "true"
+    },
+    var.ssc_cbrid_tag_value != "" ? { (var.ssc_cbrid_tag_key) = var.ssc_cbrid_tag_value } : {}
+  )
 }
