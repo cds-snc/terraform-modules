6b5054e4bd2fbddd3fa8646a85c1b2dfdeedb680
Pat Heard <patrick.heard@cds-snc.ca>
2026-09-16 11:24:06 +0000

    fix: add public route table IDs output
    Add an output with the public route table IDs.
    
    This also adds descriptions to all outputs.

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,39 +1,54 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  description = "The ID of the VPC."
+  value       = aws_vpc.main.id
 }
 
 output "cidr_block" {
-  value = aws_vpc.main.cidr_block
+  description = "The CIDR block of the VPC."
+  value       = aws_vpc.main.cidr_block
 }
 
 output "public_ips" {
-  value = aws_eip.nat.*.public_ip
+  description = "The public IP addresses of the NAT gateways."
+  value       = aws_eip.nat.*.public_ip
 }
 
 output "public_subnet_ids" {
-  value = aws_subnet.public.*.id
+  description = "The IDs of the public subnets."
+  value       = aws_subnet.public.*.id
 }
 
 output "public_subnet_cidr_blocks" {
-  value = aws_subnet.public.*.cidr_block
+  description = "The CIDR blocks of the public subnets."
+  value       = aws_subnet.public.*.cidr_block
+}
+
+output "public_route_table_ids" {
+  description = "The IDs of the public route tables."
+  value       = aws_route_table.public.*.id
 }
 
 output "private_subnet_ids" {
-  value = aws_subnet.private.*.id
+  description = "The IDs of the private subnets."
+  value       = aws_subnet.private.*.id
 }
 
 output "private_subnet_cidr_blocks" {
-  value = aws_subnet.private.*.cidr_block
+  description = "The CIDR blocks of the private subnets."
+  value       = aws_subnet.private.*.cidr_block
 }
 
 output "private_route_table_ids" {
-  value = aws_route_table.private.*.id
+  description = "The IDs of the private route tables."
+  value       = aws_route_table.private.*.id
 }
 
 output "main_nacl_id" {
-  value = aws_network_acl.main.id
+  description = "The ID of the main network ACL."
+  value       = aws_network_acl.main.id
 }
 
 output "main_route_table_id" {
-  value = aws_vpc.main.main_route_table_id
+  description = "The ID of the main route table associated with the VPC."
+  value       = aws_vpc.main.main_route_table_id
 }
